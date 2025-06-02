458d74c7b9a595e320edc01f152f777f3a91ca72
Zayed Hossain <zayedhossain120@gmail.com>
2025-06-02 16:33:25 +0600

    added student module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, AuthModule, ConfigModule.forRoot(), StudentModule],
   controllers: [AppController],
   providers: [AppService],
 })
